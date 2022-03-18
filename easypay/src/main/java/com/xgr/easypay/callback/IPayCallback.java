7a3e1c6133abaa93e32786015a901f45d0749a1f
simple <383559698@qq.com>
2022-03-18 11:13:56 +0800

    add easypay

---
+++ easypay/src/main/java/com/xgr/easypay/callback/IPayCallback.java
@@ -0,0 +1,20 @@
+package com.xgr.easypay.callback;
+
+
+import androidx.annotation.Nullable;
+
+/**
+******************************* Copyright (c)*********************************\
+**
+**                 (c) Copyright 2017, King, china
+**                          All Rights Reserved
+**                                
+**                              By(King)
+**                         
+**------------------------------------------------------------------------------
+*/
+public interface IPayCallback {
+    void success();
+    void failed(int code,@Nullable String message);
+    void cancel();
+}
